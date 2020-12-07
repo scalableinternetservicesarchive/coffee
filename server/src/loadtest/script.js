f98cde8d9226fb09769a82b2cb102517087b5a28
Huy Le <feilee2011@gmail.com>
2020-12-07 00:48:13 -0800

    Minor change script.js

--- server/src/loadtest/script.js
+++ server/src/loadtest/script.js
@@ -16,7 +16,6 @@ import { Counter, Rate } from 'k6/metrics'
 //   },
 // }
 
-
 export const options = {
   scenarios: {
     example_scenario: {
@@ -28,40 +27,24 @@ export const options = {
 }
 
 export default function () {
-  //ecordRates(http.get('http://localhost:3000/'))
   recordRates(http.get('http://localhost:3000/app/index'))
   sleep(Math.random() * 3)
   recordRates(
     http.post(
       'http://localhost:3000/graphql',
-
-      //'mutation addMenu($cafeId: Int!, $item: String!){ addMenu(cafeId:$cafeId, item:$item){ idcafeId menuDescription}}',
-      //add cafe test //'{"operationName":"AddCafe","variables":{"name":"asdaasda","long":412,"lat":231},"query":"mutation AddCafe($name: String!, $long: Float!, $lat: Float!) {\n  addCafe(name: $name, long: $long, lat: $lat) {\n    id\n    __typename\n  }\n}\n"}',
+      // loadtest AddCafe
+      //'{"operationName":"AddCafe","variables":{"name":"asdaasda","long":412,"lat":231},"query":"mutation AddCafe($name: String!, $long: Float!, $lat: Float!) {\n  addCafe(name: $name, long: $long, lat: $lat) {\n    id\n    __typename\n  }\n}\n"}',
+      // loadtest AddLike
       '{"operationName":"AddLike","variables":{"cafeId":2},"query":"mutation AddLike($cafeId: Int!) {\n  addLike(cafeId: $cafeId) {\n    id\n    __typename\n  }\n}\n"}',
+      // loadtest FetchCafes
+      {"operationName":"FetchCafes","variables":{},"query":"query FetchCafes {\n  cafes {\n    ...Cafe\n    __typename\n  }\n}\n\nfragment Cafe on Cafe {\n  id\n  name\n  longitude\n  latitude\n  __typename\n}\n"},
       {
-        headers:{
-          'Content-Type':'application/json',
+        headers: {
+          'Content-Type': 'application/json',
         },
-
       }
     )
   )
-
-  // recordRates(
-  //   const resp = http.post(
-  //   'http://localhost:3000/graphql',
-  //     'mutation addMenu($cafeId: Int!, $item: String!){ addMenu(cafeId:$cafeId, item:$item){ idcafeId menuDescription}}',
-  //     {
-  //     headers: {
-  //       '"cafeId": 1, "item"': '"my menu here"'
-  //     },
-  //     }
-  // )
-
-  // recordRates(http.get('https://cs188.cloudcity.computer/app/index'))
-  // sleep(Math.random() * 3)
-
-
 }
 
 const count200 = new Counter('status_code_2xx')
